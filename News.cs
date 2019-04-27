9cc3afca97c896a631b989936f8f059f9b3a6f3e
Joseph Spearritt <joe.spearritt@live.com>
2019-04-27 16:27:01 +1000

    News stuff

--- News.cs
+++ News.cs
@@ -4,26 +4,55 @@ using System.Linq;
 using System.Net;
 using System.Text;
 using System.Threading.Tasks;
+using Microsoft.CSharp.RuntimeBinder;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
 
 namespace TribesLauncherSharp
 {
     class News
     {
-        public string HirezLoginServerHost { get; set; }
-        public string CommunityLoginServerHost { get; set; }
+        public class NewsParsingException : Exception
+        {
+            public NewsParsingException() : base() { }
+            public NewsParsingException(string message) : base(message) { }
+            public NewsParsingException(string message, Exception inner) : base(message, inner) { }
+        }
 
-        public string LatestLauncherVersion { get; set; }
-        public string LauncherUpdateLink { get; set; }
+        public string HirezLoginServerHost { get; set; } = "23.239.17.171";
+        public string CommunityLoginServerHost { get; set; } = "ta.kfk4ever.com";
+
+        public string LatestLauncherVersion { get; set; } = "0.1.0";
+        public string LauncherUpdateLink { get; set; } = "https://raw.githubusercontent.com/mcoot/tamodsupdate/release/news.json";
 
         public static News DownloadNews(string newsUrl)
         {
             using (var wc = new WebClient())
             {
-                var rawData = wc.DownloadData(newsUrl);
-            }
+                var rawData = wc.DownloadString(newsUrl);
+                dynamic data = null;
+                try
+                {
+                    data = JObject.Parse(rawData);
+                } catch (JsonReaderException ex)
+                {
+                    throw new NewsParsingException("Failed to parse update news data", ex);
+                }
+                var n = new News();
 
-            var n = new News();
-            return n;
+                try
+                {
+                    n.LatestLauncherVersion = data.tribesLauncherSharpVersion;
+                    n.LauncherUpdateLink = data.launcherUpdateLink;
+                    n.HirezLoginServerHost = data.masterServers.hirezMasterServerHost;
+                    n.CommunityLoginServerHost = data.masterServers.unofficialMasterServerHost;
+                } catch (RuntimeBinderException ex)
+                {
+                    throw new NewsParsingException("Missing expected fields in update news data", ex);
+                }
+
+                return n;
+            }
         }
     }
 }
