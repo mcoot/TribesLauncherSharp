86fc9b1b98c85323dfd9a7b16d6c59343295c788
chickenbellyfin <chickenbellyfinn@gmail.com>
2021-09-23 21:53:01 -0700

    add injector standalone app

---
+++ InjectorStandalone/InjectorStandalone.cs
@@ -0,0 +1,21 @@
+﻿using System;
+
+namespace InjectorStandalone
+{
+    class InjectorStandalone
+    {
+        static void Main(string[] args)
+        {
+            if (args.Length < 2)
+            {
+                Console.WriteLine("Usage: InjectorStandalone <PID> <dllPath>");
+                System.Environment.Exit(1);
+            }
+
+            int pid = int.Parse(args[0]);
+            string dllPath = args[1];
+
+            TribesLauncherSharp.InjectorLauncher.Inject(pid, dllPath);
+        }
+    }
+}
